d3df8fc496ff80eda70705f3543de31513ef1358
nathidaum <nathidaum@gmail.com>
2024-10-10 16:21:21 +0200

    Finished first 3 iterations

--- src/index.js
+++ src/index.js
@@ -5,31 +5,35 @@ let timer = null; // Variable to store the interval
 
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
-
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startCountdown)
 
 // ITERATION 2: Start Countdown
+const secondsLeft = document.getElementById("time");
+
 function startCountdown() {
   console.log("startCountdown called!");
-
+  startButton.disabled = true;
 
   // Your code goes here ...
-}
+  const id = setInterval(() => {
+    remainingTime--;
+    secondsLeft.innerText = remainingTime;
 
-
-
+    if(remainingTime <= 0) {
+      clearInterval(id),
+      showToast()}
+    },
+    1000);
+  }
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
+  
+  const toaster = document.getElementById("toast"); 
+  toaster.style.display = "block";
+  toaster.classList.add("show");
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
